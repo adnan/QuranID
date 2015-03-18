dd81df01c36ca2f0ffc765802b1b72c4605ea8d2
Saif <saifmahamood@gmail.com>
2015-03-18 19:26:18 -0400

    Basic search query functional.

--- sampleSearch.py
+++ sampleSearch.py
@@ -1,138 +1,59 @@
 # coding: utf-8
 
-"""
-This is a test module for most of features provided by alfanous.main module.
-"""
-import profile
-
-from alfanous.main import QuranicSearchEngine, FuzzyQuranicSearchEngine
-from alfanous.main import TraductionSearchEngine
-from alfanous.main import WordSearchEngine
-from alfanous.main import QPaginate
-#from alfanous.ResultsProcessing import QFilter
 
 
+# import Output object
+from alfanous.Outputs import Raw
 
-if __name__ == '__main__':
-    
-    QSE = QuranicSearchEngine( "alfanous/indexes/main/" )
-    FQSE = FuzzyQuranicSearchEngine( "alfanous/indexes/main/" )
-    TSE = TraductionSearchEngine( "alfanous/indexes/extend/" )
-    QWSE = WordSearchEngine( "alfanous/indexes/word/" )
+# import default Paths
+from alfanous.Data import Paths
 
-    if QWSE.OK:
-        print "most frequent vocalized words"
-        MFW = QWSE.most_frequent_words( 10, "word" )
-        for term in MFW:
-            print "\t", term[1], " - frequence = ", term[0], "."
-        print "most  frequent unvocalized words"
-        MFW = QWSE.most_frequent_words( 10, "normalized" )
-        for term in MFW:
-            print "\t", term[1], " - frequence = ", term[0], "."
+# Initialize search engines
+RAWoutput = Raw(
+                    QSE_index = Paths.QSE_INDEX    , # Quranic Main index path
+                    TSE_index = Paths.TSE_INDEX,  # Translations index path
+                    WSE_index = Paths.WSE_INDEX,  # Quranic words index path
+                    Recitations_list_file = Paths.RECITATIONS_LIST_FILE,
+                    Translations_list_file = Paths.TRANSLATIONS_LIST_FILE ,
+                    Hints_file = Paths.HINTS_FILE,
+                    Stats_file = Paths.STATS_FILE,
+                    Information_file = Paths.INFORMATION_FILE
+                )
 
+ ## prepare a suggestion query
+suggest_flags = {
+                    "action":"suggest",
+                    "query": "ابراهيم"
+                }
+results = RAWoutput.do( suggest_flags )
 
-        RESULTS, TERMS = QWSE.search_all( "word_id:1", 
-                                       limit = 6236, 
-                                       sortedby = "score", 
-                                       reverse = True )
-        print len( RESULTS )
-
-        print "\n#list field stored VALUES# type"
-        print ",".join( [str( item ) for item in QWSE.list_values( "type" )] )
-
-    if QSE.OK:
-        print "\n#most frequent words#"
-
-        MFW = QSE.most_frequent_words( 9999999, "uth_" )
-        print len( MFW )
-        MFW_CSVFILE = open( "./uthmani_vocalized.csv", "w+" )
-        for term in MFW:
-            pass
-            #print "\t", term[1], " - frequence = ", term[0], "."
-            #print>>MFW_CSVFILE,"\t", term[1]," ;\t",term[0],"\n"
+print "number of missed words", len(results["suggest"])
 
 
+ ## prepare a search query
+search_flags = {
+                   "action":"search",
+                   "query": "الكوثر",
+                   "sortedby":"score",
+                   "page": 1,
+                   "word_info":True,
+                   "highlight":"css",
+                   "script": "standard",
+                   "prev_aya": True,
+                   "next_aya": True,
+                   "sura_info": True,
+                   "aya_position_info":  True,
+                   "aya_theme_info":  True,
+                   "aya_stat_info":  True,
+                   "aya_sajda_info":  True,
+                   "translation":1,
+                   "recitation": 1
+                }
 
-        print "\n#list field stored values#"
-        print ",".join( [str( item ) for item in QSE.list_values( "gid" )] )
+results = RAWoutput.do( search_flags )
 
-
-
-    if TSE.OK:
-        print "\n#extended search#",
-        RESULTS = TSE.find_extended( u"gid:1 OR gid:2", defaultfield = "gid" )
-        print "\n".join( [str( result ) for result in RESULTS] )
-
-        print "\n#list all translations id#"
-        print ",".join( TSE.list_values( "id" ) )
-
-
-
-    QUERY1 = "ابراهيم"
-    # >>الأمل
-    # %المأصدة
-    # لله
-    # ال*لك
-    # رب     
-    # "رسول * الله"  
-    # الصلاة وليس الزكاة
-    # #السعير  
-    # ~السعير
-    # نعمت
-    #رقم_السورة:[1 الى 5] و الله 
-    #آية_:'مَن '
-    # {ملك,فعل}
-    # >>سماكم
-    #>سماكم
-    #سماكم
-    #سجدة:نعم
-    #fawoqa
-    #\" رب العالمين\"
-    #جزء:8
-
-    if QSE.OK:
-        print "#suggestions#"
-        for key, value in QSE.suggest_all( QUERY1 ).items():
-            print key, ":", ",".join( value )
-        print "\n#search#"
-        RESULTS, TERMS = QSE.search_all( QUERY1, 
-                                     limit = 6236, 
-                                     sortedby = "score", 
-                                     reverse = True )
-
-
-        for key, freq in RESULTS.key_terms("aya", docs=1, numterms=15000): 
-            print key, "(", freq, "),",
-
-
-
-        ## test filtering results
-        #QUERY2 = u"عاصم"
-        #RESULTS2,TERMS2  = QSE.search_all(QUERY2, sortedby="mushaf")
-        #RESULTS=QFilter(RESULTS,RESULTS2)
-        #TERMS = list( set(TERMS) &  set( TERMS2))
-        
-
-
-
-        print "\n#of#"
-        for term in TERMS:
-            print "%s  (%d in %d)," % term[1:],
-
-        VALUES = [term[1] for term in list( TERMS )]
-        print "\n#is#"
-        #print RESULTS.key_terms("aya")
-        HTML = u"Results(time=%f,number=%d):\n" % ( 
-                                                   RESULTS.runtime,
-                                                   len( RESULTS ) 
-                                                   )
-        for num, respage in QPaginate( RESULTS, 5 ):
-            HTML += "~~~~~page " + str( num ) + "~~~~~~\n"
-            for r in respage:
-                HTML += "(" + \
-                        str( r["aya_id"] ) + "," + \
-                        str( r["sura_id"] ) + ") :<p>" + \
-                        QSE.highlight( r["aya_"], VALUES, "html" ) + u"</p>\n"
-            print HTML
-            HTML = ""
-            raw_input( "press any key..." )
+print(results["search"]["translation_info"])
+print "runtime", results["search"]["runtime"]
+print "total", results["search"]["interval"]["total"]
+print(results["search"]["ayas"][1]["aya"]["id"])
+print(results["search"]["ayas"][1]["sura"]["id"])
